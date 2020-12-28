8aaf7a7ad2ca078c2c358c43f59e2647f918b268
zhaoting <1060191044@qq.com>
2020-12-28 22:25:06 +0800

    解决用户名和密码错误重新提示用户输入

--- src/Test.java
+++ src/Test.java
@@ -4,25 +4,30 @@ import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) throws ClassNotFoundException {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("请输入用户名：");
+        boolean bo=true;
+        while (bo) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("请输入用户名：");
 
-        String username = sc.next();
+            String username = sc.next();
 
-        System.out.println("请输入密码：");
-        String password = sc.next();
+            System.out.println("请输入密码：");
+            String password = sc.next();
 
-        //File file = new File("C:\\Users\\lenovo\\IdeaProjects\\CmdShop\\src\\users.xlsx");
-        InputStream in = Class.forName("Test").getResourceAsStream("/users.xlsx");
-        ReadExcel readExcel = new ReadExcel();
-        User users[] = readExcel.readExcel(in);
-        for (int i = 0; i < users.length; i++) {
-            if (username.equals(users[i].getUsername()) && password.equals(users[i].getPassword())) {
-                System.out.println("登录成功");
-                break;
-            } else {
-                System.out.println("登录失败");
+            //File file = new File("C:\\Users\\lenovo\\IdeaProjects\\CmdShop\\src\\users.xlsx");
+            InputStream in = Class.forName("Test").getResourceAsStream("/users.xlsx");
+            ReadExcel readExcel = new ReadExcel();
+            User users[] = readExcel.readExcel(in);
+            for (int i = 0; i < users.length; i++) {
+                if (username.equals(users[i].getUsername()) && password.equals(users[i].getPassword())) {
+                    System.out.println("登录成功");
+                    bo=false;
+                    break;
+                } else {
+                    System.out.println("登录失败");
+                }
             }
         }
     }
+
 }
