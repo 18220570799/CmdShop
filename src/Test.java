ed58b8a56dc44540c410d351331eb06c7acac0d3
zhaoting <1060191044@qq.com>
2020-12-28 22:15:00 +0800

    修改读取users.xlsx的方式

--- src/Test.java
+++ src/Test.java
@@ -1,20 +1,21 @@
 import java.io.File;
+import java.io.InputStream;
 import java.util.Scanner;
 
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException {
         Scanner sc = new Scanner(System.in);
         System.out.println("请输入用户名：");
 
         String username = sc.next();
-        System.out.println("输入的用户名是：" + username);
 
         System.out.println("请输入密码：");
         String password = sc.next();
-        System.out.println("输入的密码是：" + password);
-        File file = new File("C:\\Users\\lenovo\\IdeaProjects\\CmdShop\\src\\users.xlsx");
+
+        //File file = new File("C:\\Users\\lenovo\\IdeaProjects\\CmdShop\\src\\users.xlsx");
+        InputStream in = Class.forName("Test").getResourceAsStream("/users.xlsx");
         ReadExcel readExcel = new ReadExcel();
-        User users[] = readExcel.readExcel(file);
+        User users[] = readExcel.readExcel(in);
         for (int i = 0; i < users.length; i++) {
             if (username.equals(users[i].getUsername()) && password.equals(users[i].getPassword())) {
                 System.out.println("登录成功");
