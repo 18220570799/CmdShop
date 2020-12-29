7d37413e087a97d9a06bac299460066c4ea14c3f
zhaoting <1060191044@qq.com>
2020-12-29 18:13:42 +0800

    测试ReadProductExcel.java

--- src/ReadProductExcel.java
+++ src/ReadProductExcel.java
@@ -9,6 +9,15 @@ import java.io.InputStream;
 import java.text.DecimalFormat;
 
 public class ReadProductExcel {
+    public static void main(String[] args) throws ClassNotFoundException {
+        InputStream in =Class.forName("Test").getResourceAsStream("/product.xlsx");
+        Product [] products=new ReadProductExcel().readExcel(in);
+        for (Product product:products){
+            System.out.print(product.getName());
+            System.out.print("\t"+product.getPrice());
+            System.out.println("\t"+product.getDescription());
+        }
+    }
     public Product[] readExcel(InputStream in) {
         Product products[] = null;
         try {
